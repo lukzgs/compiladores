e2c6dbd20ce7b69a04d3232149eb9b88138d5692
Lucas G Silveira <lucgs88@gmail.com>
2024-12-06 11:10:54 -0300

    functions for iloc

--- etapa5/include/iloc.h
+++ etapa5/include/iloc.h
@@ -1,5 +1,7 @@
 /* Lucas Guaitanelli da Silveira - 208695 */
 /* Vithor Barros Pileco - 326674 */
+#ifndef _ILOC_H_
+#define _ILOC_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,20 +14,29 @@
 // arg2 e arg3 => opcionais
 
 // Estrutura para representar uma operação ILOC
-typedef struct Iloc {
+typedef struct iloc_op {
   char* mnemonico;
   char* arg1;
   char* arg2;
   char* arg3;
-} Iloc;
+} iloc_op;
 
 // Estrutura para manter uma lista de operações ILOC
-typedef struct IlocOperationList {
-	Iloc* operation;
-	struct IlocOperationList* next_operation;
-} IlocOperationList;
+typedef struct iloc_op_list {
+	iloc_op* operation;
+	struct iloc_op_list* next_operation;
+} iloc_op_list;
 
+// Cria uma nova operação ILOC
+iloc_op* new_iloc_operation(char* mnemonico, char* arg1, char* arg2, char* arg3);
 
+// Adiciona uma operação à lista de operações ILOC
+iloc_op_list* add_iloc_operation(iloc_op* new_iloc_op);
 
-// [SEGUIR DAQUI]
-// next episode => operations 
+// Imprime uma operação ILOC
+void print_iloc_op(iloc_op* operation);
+
+// Imprime a lista de operações ILOC da lista global
+void print_iloc_op_list();
+
+#endif
---
+++ etapa5/include/test_iloc.h
@@ -0,0 +1,9 @@
+#ifndef _TEST_ILOC_H_
+#define _TEST_ILOC_H_
+
+void test_new_iloc_operation();
+void test_add_iloc_operation();
+void test_print_iloc_op();
+void test_print_iloc_op_list();
+
+#endif
